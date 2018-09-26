6b7c5b144e95f5890f8dc9c485cefd6718416825
Federico Altieri <calebtheone@hotmail.it>
2018-09-26 11:53:41 +0200

    Added demo datasts for cross validation checks

---
+++ DEMOCrossVal.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./nomas.sh data datasets/demoDataset.txt crossval k 3 N 4 iterations 2000 solutions 10 threshold 0 output DEMOCROSSVAL timeSplits 4 proportion 0.5
---
+++ DEMOCrossVal2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./nomas.sh data datasets/demoDataset.txt k 3 iterations 2000 solutions 1 threshold 0 output DEMOCROSSVAL
